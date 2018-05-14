0671e04c3148ba93420d576f05402dcfb0b9dd75
Jessica Perkins <jnperk1234@gmail.com>
2018-05-14 00:37:32 -0500

    upd1

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE `burgers_db`;
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Swiss Bacon Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Cheeseburger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Hamburger', false, CURRENT_TIMESTAMP);
--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 var express = require("express")
+var methodOverride = require("method-overide");
+var bodyParser = require("body-parser");
+
 var app= express ()
 
 var port= process.env.PORT||3000
 
-var data= ["bear", "cat", "dog", "unicorn"]
+app.use(express.static(__dirname = '/public'));
 
-app.get("/", function(req, res){
-    res.send("app is working")
-});
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json'}));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
+app.use(bodyParser.text({ type: 'text/html'}));
 
-app.get("/apple", function(req, res){
-    res.send(data)
+app.use(methodOverride('_method'));
+
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+app.listen(PORT, function(){
+    console.log("App Listening on PORT" = PORT);
 })
-app.listen(port)
+
+
+//var data= ["bear", "cat", "dog", "unicorn"]
+
+//app.get("/", function(req, res){
+    res.send("app is working")
+//});
+
+//app.get("/apple", function(req, res){
+    res.send(data)
+//})
+//app.listen(port)
 
